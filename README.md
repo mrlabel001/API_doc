ef11dc7b106fc6908588c864638b17342d059f8e
mrlabel001 <farbean.makini@student.moringaschool.com>
2024-06-21 15:17:37 +0300

    Navigation_tabs

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# API_doc
+# API_ddocumentation webapp
+ The webapp is a comprehensive tool for accessing and integrating data from two powerful APIs: Unsplash API and Rick and Morty API. This web application allows users to seamlessly explore a vast array of high-quality images from Unsplash, perfect for creative projects and visual inspiration. Additionally, it provides access to a wealth of information and character details from the Rick and Morty universe, ideal for fans and developers interested in building applications around this popular animated series
+
+# Table of content
+Even though both APIs are documented in the same app, they all are differently explained in the following order:
+1. Overview/ description
+2. Authentication
+3. Rate limiting
+4. Error codes
+5. Endpoints
--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,14 +1,40 @@
-import './App.css'
-import UnSplash from './UnSplash/UnSplash'
-import RickMorty from './RickMorty/RickMorty'
+import React, { useState } from 'react';
+import './App.css';
+import UnSplash from './UnSplash/UnSplash';
+import RickMorty from './RickMorty/RickMorty';
 
-function App () {
+function App() {
+  const [activeTab, setActiveTab] = useState('unsplash');
+
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  };
+
   return (
     <>
-      <UnSplash/>
-      <RickMorty/>
+    <h1>API DOCUMENTATION</h1>
+    <div className="app">
+      <div className="tabs">
+        <button
+          className={activeTab === 'unsplash' ? 'active' : ''}
+          onClick={() => handleTabClick('unsplash')}
+        >
+          UnSplash
+        </button>
+        <button
+          className={activeTab === 'rickmorty' ? 'active' : ''}
+          onClick={() => handleTabClick('rickmorty')}
+        >
+          RickMorty
+        </button>
+      </div>
+      <div className="tab-content">
+        {activeTab === 'unsplash' && <UnSplash />}
+        {activeTab === 'rickmorty' && <RickMorty />}
+      </div>
+    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
